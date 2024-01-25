7defd0bc965633e26e15566a34061df29ecfe68c
Pratham Bhargava <77926682+prathambhargava@users.noreply.github.com>
2024-01-25 11:43:10 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello java !!!");
+        int year = 2020;
+        String winner = "india";
+        String announcemnet = "The winner of the year " + year +" is "+winner;
+        System.out.println(announcemnet);
+
+    }
+}
