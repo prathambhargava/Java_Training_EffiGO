953b534bdc5e5c02b97738003816acc0187eb772
Pratham Bhargava <bhargavapratham1@gmail.com>
2024-01-28 17:27:33 +0530

    getter

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -5,6 +5,8 @@ public class oops {
     public static void main(String[] args) {
          cons nissan = new cons("nissan",1000,2020,"green");
          cons maruti = new cons("Maruti",1200,1920,"black");
+
+         System.out.println("this " + nissan.getMake() +" is worth $ " + nissan.getPrice() +" build in " + nissan.getYear() + ". It is " + nissan.getColor() +".\n");
     }
     
 }
