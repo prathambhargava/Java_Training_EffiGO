5669fceedd5fcdff1e401e52ff33f9f599a1360f
Pratham Bhargava <bhargavapratham1@gmail.com>
2024-01-28 17:39:34 +0530

    setter

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -6,6 +6,9 @@ public class oops {
          cons nissan = new cons("nissan",1000,2020,"green");
          cons maruti = new cons("Maruti",1200,1920,"black");
 
+         nissan.setColor("jet Black");
+         nissan.setPrice(nissan.getPrice() / 2);
+
          System.out.println("this " + nissan.getMake() +" is worth $ " + nissan.getPrice() +" build in " + nissan.getYear() + ". It is " + nissan.getColor() +".\n");
     }
     
